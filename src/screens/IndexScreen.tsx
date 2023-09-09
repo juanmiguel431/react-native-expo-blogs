d22e8d9ef537c483f0c789acf6083bb3a67a8aea
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-09 15:43:50 -0400

    Style to blog list.

--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
-import { Button, FlatList, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import { IndexScreenProps } from '../models/screen';
 import { BlogContext } from '../contex/BlogContext';
 import { BLOG_ACTION_TYPE } from '../models/actions';
+import { Feather } from '@expo/vector-icons'
 
 const IndexScreen: React.FC<IndexScreenProps> = () => {
   const { state: { data }, dispatch } = useContext(BlogContext);
 
   return (
     <View>
-      <Text>Hello there!</Text>
       <Button
         title="Add Blog"
         onPress={() => dispatch({ type: BLOG_ACTION_TYPE.Add })}
@@ -18,11 +18,31 @@ const IndexScreen: React.FC<IndexScreenProps> = () => {
         data={data}
         keyExtractor={item => item.title}
         renderItem={({ item }) => (
-          <Text>{item.title}</Text>
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather name="trash" style={styles.icon}/>
+          </View>
         )}
       />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 24
+  },
+  row: {
+    borderColor: 'gray',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 20
+  },
+  title: {
+    fontSize: 18,
+  }
+});
 
 export default IndexScreen;
